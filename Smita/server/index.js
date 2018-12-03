ca6baab3db34954586d8e49a4cec4c3d1436cf56
Smita Samanta <smithers@Smitas-MacBook-Air.local>
2018-12-03 17:46:35 +0530

    Backend for users works on GraphQL

--- Smita/server/index.js
+++ Smita/server/index.js
@@ -36,6 +36,7 @@ const typeDefs = `
   type Mutation {
     createUser(username: String!, email: String!, checkAdmin: Boolean): User
     removeUser(id: ID!): Boolean
+    updateUser(id: ID!, checkAdmin: Boolean!): Boolean
   }
 `;
 
@@ -53,7 +54,11 @@ const resolvers = {
     removeUser: async (_, { id }) => {
       await User.findByIdAndRemove(id);
       return true;
-    }
+    }, 
+    updateUser: async (_, { id, checkAdmin }) => {
+      await User.findByIdAndUpdate(id, { checkAdmin });
+      return true;
+    },
   }
 };
 
@@ -63,71 +68,3 @@ mongoose.connection.once("open", function() {
 });
 
 
-
-// const { GraphQLServer } = require("graphql-yoga");
-
-// /* MONGOOSE SETUP */
-// // Configuring the database
-// const dbConfig = require('./config/database.config.js');
-// const mongoose = require('mongoose');
-// mongoose.set('useFindAndModify', false);
-
-// mongoose.Promise = global.Promise;
-
-// // Connecting to the database
-// mongoose.connect(dbConfig.url, {
-// }).then(() => {
-//     console.log("Successfully connected to the database! ");    
-// }).catch(err => {
-//     console.log('Could not connect to the database. Exiting now.');
-//     process.exit();
-// });
-
-// const Todo = mongoose.model("Todo", {
-//   text: String,
-//   complete: Boolean
-// });
-
-// const typeDefs = `
-//   type Query {
-//     hello(name: String): String!
-//     todos: [Todo]
-//   }
-//   type Todo {
-//     id: ID!
-//     text: String!
-//     complete: Boolean!
-//   }
-//   type Mutation {
-//     createTodo(text: String!): Todo
-//     updateTodo(id: ID!, complete: Boolean!): Boolean
-//     removeTodo(id: ID!): Boolean
-//   }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     hello: (_, { name }) => `Hello ${name || "World"}`,
-//     todos: () => Todo.find()
-//   },
-//   Mutation: {
-//     createTodo: async (_, { text }) => {
-//       const todo = new Todo({ text, complete: false });
-//       await todo.save();
-//       return todo;
-//     },
-//     updateTodo: async (_, { id, complete }) => {
-//       await Todo.findByIdAndUpdate(id, { complete });
-//       return true;
-//     },
-//     removeTodo: async (_, { id }) => {
-//       await Todo.findByIdAndRemove(id);
-//       return true;
-//     }
-//   }
-// };
-
-// const server = new GraphQLServer({ typeDefs, resolvers });
-// mongoose.connection.once("open", function() {
-//   server.start(() => console.log("Server is running on localhost:4000"));
-// });
